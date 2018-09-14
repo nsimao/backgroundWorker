a94aee062238467be06189d9a149e5e6d331cc45
nelsons <nelson.simao@me.com.br>
2018-09-14 11:19:01 -0300

    Project Add

---
+++ BackgroundWorkerExemplo/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace BackgroundWorkerExemplo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(99, 27);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 0;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Enabled = false;
+            this.btnCancelar.Location = new System.Drawing.Point(203, 27);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 1;
+            this.btnCancelar.Text = "Parar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.BackColor = System.Drawing.SystemColors.Control;
+            this.lblStatus.Location = new System.Drawing.Point(3, 69);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(380, 23);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "Execução:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(5, 95);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(377, 95);
+            this.listBox1.TabIndex = 3;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(5, 196);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(378, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 225);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnIniciar);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BackgroundWorker";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ BackgroundWorkerExemplo/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackgroundWorkerExemplo
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker bgWorker = null;
+        delegate void SetTextCallback(Label lbl, string texto);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ParaBackGroundWorker()
+        {
+            if (bgWorker.IsBusy)
+                bgWorker.CancelAsync();
+
+            lblStatus.Text = "Execução:";
+
+            progressBar1.Value = 0;
+
+            listBox1.Items.Clear();
+
+            btnIniciar.Enabled = true;
+            btnCancelar.Enabled = false;
+        }
+
+
+        #region ThreadSafe
+        private void SetLabel(Label lbl, string texto)
+        {
+            if (lbl.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetLabelThreadSafe);
+                this.Invoke(d, new object[] { lbl, texto });
+            }
+            else
+            {
+                lbl.Text = texto;
+            }
+        }
+
+        private void AddToListBox(string texto)
+        {
+            Invoke(new MethodInvoker(
+                           delegate { listBox1.Items.Add(texto); }
+                           ));
+        }
+
+
+        private void StepItProgressBar()
+        {
+            Invoke(new MethodInvoker(
+                           delegate { progressBar1.PerformStep(); }
+                           ));
+        }
+
+        private void SetLabelThreadSafe(Label lbl, string texto)
+        {
+            lbl.Text = texto;
+        }
+        #endregion
+
+        #region Handlers
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bgWorker = new BackgroundWorker();
+            bgWorker.WorkerSupportsCancellation = true;
+            bgWorker.DoWork += BgWorker_DoWork;
+        }
+
+        private void BgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (1 == 1)
+            {
+                string texto = "Execução em: " + DateTime.Now.ToString();
+
+                SetLabel(lblStatus, texto);
+                AddToListBox(texto);
+                StepItProgressBar();
+
+                Thread.Sleep(1000);
+
+                if (bgWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+            }
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            if (!bgWorker.IsBusy)
+            {
+                btnIniciar.Enabled = false;
+                btnCancelar.Enabled = true;
+                bgWorker.RunWorkerAsync();
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            ParaBackGroundWorker();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            ParaBackGroundWorker();
+        }
+        #endregion
+    }
+}
